3476b507609054592ed621806f4dcaf31f6464ff
Joel Guerreiro <25035669+Joelfo123@users.noreply.github.com>
2019-07-31 14:19:41 -0300

    Update Somador.java

--- Somador.java
+++ Somador.java
@@ -3,23 +3,22 @@ import java.util.Scanner;
 
 public Class Somador
 {
-  public static void main(String[] args)
-  {
-    Scanner input = New Scanner (System.in);
+    public static void main(String[] args)
+    {
+        Scanner input = New Scanner (System.in);
     
-    int numero1;
-    int numero2;
-    int soma;
+        int numero1;
+        int numero2;
+        int soma;
     
-    System.out.println("Entre com o primeiro numero por favor:");
-    numero1 = input.nextInt();
+        System.out.println("Entre com o primeiro numero por favor:");
+        numero1 = input.nextInt();
     
-    System.out.println("Entre com o segundo numero por favor
-    numero2 = input.nextInt();
+        System.out.println("Entre com o segundo numero por favor
+        numero2 = input.nextInt();
     
-    soma = numero1 + numero2;
+        soma = numero1 + numero2;
     
-    System.out.println("A soma dos dois numeros deu: %d%n ", soma);
-    
-  }
+        System.out.println("A soma dos dois numeros deu: %d%n ", soma);
+     }
 }
