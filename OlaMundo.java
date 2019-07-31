294c4b583907f166d29824a885e247e462862a46
Joel Guerreiro <25035669+Joelfo123@users.noreply.github.com>
2019-07-31 14:01:40 -0300

    Update OlaMundo.java

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,10 +1,9 @@
-//Programinha que escreve na tela
+//Programinha que escreve mensagem na tela na tela
 
 public class OlaMundo
 {
-    //main
-    public static void main(String[] args)
+    public static void main(String [] args)
     {
-	      System.out.println("Ola mundo!");
+        System.out.println("Ola mundo!");
     }
 }
