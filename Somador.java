beb432d676773baeea0496bd62c9120025eae35d
Joel Guerreiro <25035669+Joelfo123@users.noreply.github.com>
2019-07-31 14:16:53 -0300

    Create Somador.java

---
+++ Somador.java
@@ -0,0 +1,25 @@
+//Programa que recebe dois números inteiros e mostra a soma na tela
+import java.util.Scanner;
+
+public Class Somador
+{
+  public static void main(String[] args)
+  {
+    Scanner input = New Scanner (System.in);
+    
+    int numero1;
+    int numero2;
+    int soma;
+    
+    System.out.println("Entre com o primeiro numero por favor:");
+    numero1 = input.nextInt();
+    
+    System.out.println("Entre com o segundo numero por favor
+    numero2 = input.nextInt();
+    
+    soma = numero1 + numero2;
+    
+    System.out.println("A soma dos dois numeros deu: %d%n ", soma);
+    
+  }
+}
